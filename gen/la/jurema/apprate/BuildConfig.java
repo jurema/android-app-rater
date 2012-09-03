12d3243376ebdc766d6f36382ea12f10149b9754
Ademar Alves de Oliveira <ademar111190@gmail.com>
2012-09-03 11:32:48 -0300

    'adaptando para o java 7'

---
+++ gen/la/jurema/apprate/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package la.jurema.apprate;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/la/jurema/apprater/manager/Rate.java
+++ src/la/jurema/apprater/manager/Rate.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class Rate implements DialogInterface.OnCancelListener{
+public class Rate implements DialogInterface.OnCancelListener {
 
 	public static Context context;
 	public static String packpageInMarket;
@@ -28,7 +28,7 @@ public class Rate implements DialogInterface.OnCancelListener{
 	public static Integer opensIntervalFirst;
 	public static boolean showInSecion;
 	private static Dialog alertRate;
-	
+
 	/**
 	 * 
 	 * @param context
@@ -62,10 +62,11 @@ public class Rate implements DialogInterface.OnCancelListener{
 	 * @param saveHistory
 	 *            send true to save historic in sqlite default is false
 	 */
-	public static void configure(Context context, int daysIntervalFirst, int opensIntervalFirst,
-			int daysInterval, int opensInterval,
+	public static void configure(Context context, int daysIntervalFirst,
+			int opensIntervalFirst, int daysInterval, int opensInterval,
 			String packpageInMarket, String title, String message,
-			String rateText, String remindLaterText, String noRateText, boolean showLog) {
+			String rateText, String remindLaterText, String noRateText,
+			boolean showLog) {
 		Rate.context = context;
 		Rate.packpageInMarket = packpageInMarket;
 		Rate.title = title;
@@ -78,104 +79,108 @@ public class Rate implements DialogInterface.OnCancelListener{
 		Rate.daysIntervalFirst = daysIntervalFirst;
 		Rate.opensIntervalFirst = opensIntervalFirst;
 		Rate.showInSecion = false;
-		
+
 		LogManager.setLogEnable(showLog);
 		LogManager.print("Opening analyzer");
 		Rate.rateAnalyzer = new RateAnalyzer(context);
 	}
-	
-	public static void onCreate(){
+
+	public static void onCreate() {
 		rateAnalyzer.make();
 	}
 
-	public static void onResume(){
+	public static void onResume() {
 		rateAnalyzer.make();
 	}
-	
-	public static void onDestroy(){
+
+	public static void onDestroy() {
 		rateAnalyzer.close();
 	}
 
 	public static void showDialog() {
-		if(!showInSecion){
+		if (!showInSecion) {
 			showInSecion = true;
-			
-			alertRate = (title!=null?new Dialog(context):new Dialog(context,R.style.CustomDialogTheme));
+
+			alertRate = (title != null ? new Dialog(context) : new Dialog(
+					context, R.style.CustomDialogTheme));
 			alertRate.setContentView(R.layout.app_rater_message);
 			alertRate.setCancelable(true);
 			alertRate.setOnCancelListener(new Rate());
-			
+
 			Button btnPositive = (Button) alertRate.findViewById(R.id.btn_vote);
 			Button btnNeutral = (Button) alertRate.findViewById(R.id.btn_later);
-			Button btnNegative = (Button) alertRate.findViewById(R.id.btn_never);
-			TextView txtMessage = (TextView) alertRate.findViewById(R.id.txt_message);
-			
-			LinearLayout.LayoutParams lpInvisible = new LinearLayout.LayoutParams(0, 0);
-			
-			if(title!=null){
+			Button btnNegative = (Button) alertRate
+					.findViewById(R.id.btn_never);
+			TextView txtMessage = (TextView) alertRate
+					.findViewById(R.id.txt_message);
+
+			LinearLayout.LayoutParams lpInvisible = new LinearLayout.LayoutParams(
+					0, 0);
+
+			if (title != null) {
 				alertRate.setTitle(title);
 			}
-			if(message!=null){
+			if (message != null) {
 				txtMessage.setText(message);
-			}else{
+			} else {
 				txtMessage.setLayoutParams(lpInvisible);
 			}
-			if(rateText!=null){
+			if (rateText != null) {
 				btnPositive.setText(rateText);
 				btnPositive.setOnClickListener(new View.OnClickListener() {
-					@Override
 					public void onClick(View v) {
-						LogManager.print(rateText+" selected");
-						rateAnalyzer.resultToHistory(RateAnalyzer.TYPE_SHOW_RATE);
-						if(packpageInMarket!=null){
-							try{
+						LogManager.print(rateText + " selected");
+						rateAnalyzer
+								.resultToHistory(RateAnalyzer.TYPE_SHOW_RATE);
+						if (packpageInMarket != null) {
+							try {
 								Intent intent = new Intent(Intent.ACTION_VIEW);
-								intent.setData(Uri.parse("market://details?id="+packpageInMarket));
+								intent.setData(Uri.parse("market://details?id="
+										+ packpageInMarket));
 								context.startActivity(intent);
-							}catch (Exception e) {
+							} catch (Exception e) {
 								LogManager.print("Device dont have market app");
 							}
-						}else{
+						} else {
 							LogManager.print("packpage is null");
 						}
 						alertRate.cancel();
 					}
 				});
-			}else{
+			} else {
 				btnPositive.setLayoutParams(lpInvisible);
 			}
-			if(remindLaterText!=null){
+			if (remindLaterText != null) {
 				btnNeutral.setText(remindLaterText);
 				btnNeutral.setOnClickListener(new View.OnClickListener() {
-					@Override
 					public void onClick(View v) {
-						LogManager.print(remindLaterText+" selected");
-						rateAnalyzer.resultToHistory(RateAnalyzer.TYPE_SHOW_LATER);
+						LogManager.print(remindLaterText + " selected");
+						rateAnalyzer
+								.resultToHistory(RateAnalyzer.TYPE_SHOW_LATER);
 						alertRate.cancel();
 					}
 				});
-			}else{
+			} else {
 				btnNeutral.setLayoutParams(lpInvisible);
 			}
-			if(noRateText!=null){
+			if (noRateText != null) {
 				btnNegative.setText(noRateText);
 				btnNegative.setOnClickListener(new View.OnClickListener() {
-					@Override
 					public void onClick(View v) {
-						LogManager.print(noRateText+" selected");
-						rateAnalyzer.resultToHistory(RateAnalyzer.TYPE_SHOW_CANCEL);
+						LogManager.print(noRateText + " selected");
+						rateAnalyzer
+								.resultToHistory(RateAnalyzer.TYPE_SHOW_CANCEL);
 						alertRate.cancel();
 					}
 				});
-			}else{
+			} else {
 				btnNegative.setLayoutParams(lpInvisible);
 			}
-			
+
 			alertRate.show();
 		}
 	}
-	
-	@Override
+
 	public void onCancel(DialogInterface dialog) {
 		LogManager.print("Canceled box");
 		rateAnalyzer.resultToHistory(RateAnalyzer.TYPE_SHOW_BACK);
